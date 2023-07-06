32a8a7b1cf24bfecbb3232ea96f679acd612b1ee
BinayPrad <115228155+BinayPrad@users.noreply.github.com>
2023-07-06 18:18:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,75 @@
+import os
+os.environ["OPENAI_API_KEY"] = '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'
+
 import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
-from hugchat import hugchat
+from llama_index import download_loader
+from llama_index.node_parser import SimpleNodeParser
+from llama_index import GPTVectorStoreIndex
+from llama_index import LLMPredictor, GPTVectorStoreIndex, PromptHelper, ServiceContext
+from langchain import OpenAI
 
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+doc_path = './data/'
+index_file = 'index.json'
 
-# Sidebar contents
-with st.sidebar:
-    st.title('🤗💬 HugChat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
-    
-    💡 Note: No API key required!
-    ''')
-    add_vertical_space(5)
-    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
+if 'response' not in st.session_state:
+    st.session_state.response = ''
 
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
+def send_click():
+    st.session_state.response  = index.query(st.session_state.prompt)
 
-# Layout of input/response containers
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
+index = None
+st.title("Binay's Doc Chatbot")
 
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()
+sidebar_placeholder = st.sidebar.container()
+uploaded_file = st.file_uploader("Choose a file")
 
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
-    chatbot = hugchat.ChatBot()
-    response = chatbot.chat(prompt)
-    return response
+if uploaded_file is not None:
 
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+    doc_files = os.listdir(doc_path)
+    for doc_file in doc_files:
+        os.remove(doc_path + doc_file)
+
+    bytes_data = uploaded_file.read()
+    with open(f"{doc_path}{uploaded_file.name}", 'wb') as f:
+        f.write(bytes_data)
+
+    SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+
+    loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
+    documents = loader.load_data()
+    sidebar_placeholder.header('Current Processing Document:')
+    sidebar_placeholder.subheader(uploaded_file.name)
+    sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+
+    max_input_size = 4096
+    num_output = 256
+    max_chunk_overlap = 20
+    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index = GPTVectorStoreIndex.from_documents(
+        documents, service_context=service_context
+    )
+
+    index.save_to_disk(index_file)
+
+elif os.path.exists(index_file):
+    index = GPTVectorStoreIndex.load_from_disk(index_file)
+
+    SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+    loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
+    documents = loader.load_data()
+    doc_filename = os.listdir(doc_path)[0]
+    sidebar_placeholder.header('Current Processing Document:')
+    sidebar_placeholder.subheader(doc_filename)
+    sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+
+if index != None:
+    st.text_input("Ask something: ", key='prompt')
+    st.button("Send", on_click=send_click)
+    if st.session_state.response:
+        st.subheader("Response: ")
+        st.success(st.session_state.response, icon= "🤖")
